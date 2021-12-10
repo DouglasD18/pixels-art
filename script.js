8e2b141926c8ffec42ca3b0ad2d8467f8245c43e
Douglas Aguiar <daguiaralcantara@gmail.com>
2021-12-10 11:17:03 -0300

    Continuação...

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-/* O quadro de "pixels" deve ter 5 elementos de largura e 5 elementos de comprimento;
-O quadro de "pixels" deve possuir o id denominado pixel-board, ao passo que cada "pixel" individual dentro do quadro deve possuir a classe denominada pixel;
-A cor inicial dos "pixels" dentro do quadro, ao abrir a página, deve ser branca;
-O quadro de "pixels" deve aparecer abaixo da paleta de cores.
-O que será verificado:
-Verifica se o quadro de pixels possui o id denominado pixel-board
-Verifica se cada pixel individual dentro do quadro possui a classe denominada pixel.
-Verifica se a cor inicial dos pixels dentro do quadro, ao abrir a página, é branca.
-Verifica se o quadro de pixels aparece abaixo da paleta de cores */
-
 let quadro = document.getElementById('pixel-board');
+quadro.style.display = 'block';
+quadro.style.height = '204px'
 let vertical = 1;
 let horizontal = 1;
 
@@ -26,11 +18,15 @@ for (let i = 0; i < 5; i++) {
 
 let linhas = document.getElementsByClassName('linha');
 for (let i = 0; i < linhas.length; i++) {
-    linhas[i].style.display = 'inline';
+    linhas[i].style.height = '42px';
+    linhas[i].style.width = '210px';
 }
 
 let estilo = document.getElementsByClassName('pixel');
 for (let i = 0; i < estilo.length; i++) {
+    estilo[i].style.display = 'inline'
     estilo[i].style.backgroundColor = 'white';
-    estilo[i].style.padding = '25px';
+    estilo[i].style.width = '40px';
+    estilo[i].style.height = '40px';
+    estilo[i].style.border = '1px solid black';
 }
