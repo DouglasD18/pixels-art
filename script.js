54248666745d15a26e95631121f8ffdba124f1a7
Douglas Aguiar <daguiaralcantara@gmail.com>
2021-12-10 11:56:26 -0300

    Resolução do quinto requisito.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let quadro = document.getElementById('pixel-board');
-quadro.style.display = 'block';
-quadro.style.height = '204px'
+
 let vertical = 1;
 let horizontal = 1;
 
@@ -14,19 +13,4 @@ for (let i = 0; i < 5; i++) {
         div.appendChild(pixel);
         horizontal += 1;
     }
-}
-
-let linhas = document.getElementsByClassName('linha');
-for (let i = 0; i < linhas.length; i++) {
-    linhas[i].style.height = '42px';
-    linhas[i].style.width = '210px';
-}
-
-let estilo = document.getElementsByClassName('pixel');
-for (let i = 0; i < estilo.length; i++) {
-    estilo[i].style.display = 'inline'
-    estilo[i].style.backgroundColor = 'white';
-    estilo[i].style.width = '40px';
-    estilo[i].style.height = '40px';
-    estilo[i].style.border = '1px solid black';
 }
