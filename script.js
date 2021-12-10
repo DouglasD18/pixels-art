6b144d11315212794ae17e3cde59a3de2e5d5971
Douglas Aguiar <daguiaralcantara@gmail.com>
2021-12-10 13:01:06 -0300

    Resolução do sétimo requisito.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let quadro = document.getElementById('pixel-board');
-
 let vertical = 1;
 let horizontal = 1;
+let colorPalette = document.getElementById('color-palette');
+let color = document.getElementsByClassName('color');
 
 for (let i = 0; i < 5; i++) {
     let div = document.createElement('div')
@@ -14,3 +15,27 @@ for (let i = 0; i < 5; i++) {
         horizontal += 1;
     }
 }
+
+function removeSelected() {
+    for (let i = 0; i < color.length; i++) {
+        if (color[i].classList[1] == 'selected') {
+            color[i].classList.remove('selected');
+            break;
+        }
+    }
+}
+
+function adicionaSelected(clicado) {
+    clicado.classList.add('selected');
+}
+
+function trocaSelected(event) {
+    removeSelected();
+    let clicado = event.target;
+    adicionaSelected(clicado);
+}
+
+color[0].addEventListener('click', trocaSelected);
+color[1].addEventListener('click', trocaSelected);
+color[2].addEventListener('click', trocaSelected);
+color[3].addEventListener('click', trocaSelected);
